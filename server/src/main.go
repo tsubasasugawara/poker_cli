c55359430b9a20d552d3353b7f2de9bbae1df44b
tsubasasugawara <sugawara1740@gmail.com>
2022-09-07 22:11:29 +0900

    Add
    Added router.

--- server/src/main.go
+++ server/src/main.go
@@ -1,44 +1,13 @@
-
 package main
 
 import (
-	"net/http"
-	"poker/controller/user"
-	"poker/controller/rooms"
-
     "github.com/gin-gonic/gin"
 )
 
 func main() {
 	engine := gin.Default()
 
-	var ua string
-	engine.Use(func(c *gin.Context) {
-		ua = c.GetHeader("User-Agent")
-		c.Next()
-	})
-
-	userEngine := engine.Group("/user")
-	{
-		userEngine.POST("/regist", user.Regist)
-		userEngine.POST("/login", user.Login)
-		userEngine.POST("/delete", user.Delete)
-		userEngine.POST("/edit", user.Edit)
-	}
-
-	roomEngine := engine.Group("/room")
-	{
-		roomEngine.POST("/create", rooms.CreateRoom)
-		roomEngine.POST("/participate", rooms.ParticipateRoom)
-		roomEngine.POST("/exit", rooms.ExitRoom)
-	}
-
-	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-			"User-Agent": ua,
-		})
-	})
+	router(engine)
 
     engine.Run(":8080")
 }
---
+++ server/src/router.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"poker/controller/user"
+	"poker/controller/rooms"
+
+    "github.com/gin-gonic/gin"
+)
+
+func router(engine *gin.Engine) {
+	userEngine := engine.Group("/user")
+	{
+		userEngine.POST("/regist", user.Regist)
+		userEngine.POST("/login", user.Login)
+		userEngine.POST("/delete", user.Delete)
+		userEngine.POST("/edit", user.Edit)
+	}
+
+	roomEngine := engine.Group("/room")
+	{
+		roomEngine.POST("/create", rooms.CreateRoom)
+		roomEngine.POST("/participate", rooms.ParticipateRoom)
+		roomEngine.POST("/exit", rooms.ExitRoom)
+	}
+
+	playEngine := engine.Group("/play")
+	{
+		playEngine.POST("/deal")
+		playEngine.POST("action")
+
+	}
+}
